536c2a099e8e1df61e16d5b596563275cb98342b
ssaberan <ssaberan96@gmail.com>
2021-07-04 18:39:44 -0400

    remove markdown in favor of jsx for blog posts

--- src/Pages/Blog/BlogPosts/Post1/fullPost.jsx
+++ src/Pages/Blog/BlogPosts/Post1/fullPost.jsx
@@ -1,31 +1,7 @@
-import React, { useState, useEffect } from "react";
-import ReactMarkdown from "react-markdown";
-import gfm from "remark-gfm";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
-import "katex/dist/katex.min.css";
+import rawPost from "./rawPost";
 
 const Post1 = () => {
-   const [state, setState] = useState({ terms: null });
-
-   useEffect(() => {
-      fetch("/src/Pages/Blog/BlogPosts/Post1/markdownPost.md")
-         .then((response) => response.text())
-         .then((text) => {
-            setState({ terms: text });
-         });
-   }, []);
-
-   return (
-      <>
-         <ReactMarkdown
-            remarkPlugins={[gfm, remarkMath]}
-            rehypePlugins={[rehypeKatex]}
-         >
-            {state.terms}
-         </ReactMarkdown>
-      </>
-   );
+   return rawPost();
 };
 
 export default Post1;
---
+++ src/Pages/Blog/BlogPosts/Post1/rawPost.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const rawPost = () => {
+   return (
+      <div>
+         <h1>Post 1</h1>
+      </div>
+   );
+};
+
+export default rawPost;
