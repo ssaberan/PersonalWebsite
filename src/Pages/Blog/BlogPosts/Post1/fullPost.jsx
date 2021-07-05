f6f58457dd6a862e70dc33d05f38a172851f70af
ssaberan <ssaberan96@gmail.com>
2021-07-05 00:22:45 -0400

    blost post styling

--- src/Pages/Blog/BlogPosts/Post1/fullPost.jsx
+++ src/Pages/Blog/BlogPosts/Post1/fullPost.jsx
@@ -1,7 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
 import rawPost from "./rawPost";
 
+const StyledPage = styled.div`
+   margin: auto;
+   width: 60%;
+   padding-top: 30px;
+   padding-bottom: 50px;
+   background: white;
+
+   @media screen and (max-width: 768px) {
+      width: 75%;
+   }
+
+   @media screen and (max-width: 425px) {
+      width: 90%;
+   }
+`;
+
 const Post1 = () => {
-   return rawPost();
+   return <StyledPage>{rawPost()}</StyledPage>;
 };
 
 export default Post1;
