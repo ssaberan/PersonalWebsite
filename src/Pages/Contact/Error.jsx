57cff42a2d4ac2089c0f7f65266a909cc414840b
ssaberan <ssaberan96@gmail.com>
2021-07-10 20:21:28 -0400

    error and success results for sending email

--- src/Pages/Contact/Error.jsx
+++ src/Pages/Contact/Error.jsx
@@ -1,13 +1,49 @@
 import React from "react";
+import styled from "styled-components";
 import { Helmet } from "react-helmet";
+
+import errorImage from "../../../images/error.svg";
+
+const StyledPage = styled.div`
+   display: block;
+   margin: auto;
+   text-align: center;
+   color: white;
+   padding: 20px;
+`;
+
+const StyledBackground = styled.div`
+   background: #101522;
+   background-image: linear-gradient(#164d69, #101522);
+   top: 0;
+   bottom: 0;
+   margin: auto;
+`;
+
+const StyledImage = styled.img`
+   padding: 0;
+   display: block;
+   margin: 0 auto;
+   max-height: 100%;
+   max-width: 100%;
+`;
 
 const Error = () => {
    return (
       <>
          <Helmet>
-            <title>Error</title>
+            <title>Email Sent</title>
          </Helmet>
-         <h1>Error!</h1>
+         <StyledBackground>
+            <StyledPage>
+               <h1>Oh No! It looks like the email failed to send!</h1>
+               <h3>
+                  I&apos;m working on fixing the issue. Until then, you can
+                  email me at ssaberan96@gmail.com.
+               </h3>
+               <StyledImage src={errorImage} />
+            </StyledPage>
+         </StyledBackground>
       </>
    );
 };
--- src/Pages/Contact/Sent.jsx
+++ src/Pages/Contact/Sent.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 import { Helmet } from "react-helmet";
+
+import sentImage from "../../../images/sent.svg";
+
+const StyledPage = styled.div`
+   display: block;
+   margin: auto;
+   text-align: center;
+   color: white;
+   padding: 20px;
+`;
+
+const StyledBackground = styled.div`
+   background: #101522;
+   background-image: linear-gradient(#164d69, #101522);
+   top: 0;
+   bottom: 0;
+   margin: auto;
+`;
+
+const StyledImage = styled.img`
+   padding: 0;
+   display: block;
+   margin: 0 auto;
+   max-height: 100%;
+   max-width: 100%;
+`;
 
 const Sent = () => {
    return (
@@ -7,7 +34,12 @@ const Sent = () => {
          <Helmet>
             <title>Email Sent</title>
          </Helmet>
-         <h1>Sent!</h1>
+         <StyledBackground>
+            <StyledPage>
+               <h1>Sent!</h1>
+               <StyledImage src={sentImage} />
+            </StyledPage>
+         </StyledBackground>
       </>
    );
 };
